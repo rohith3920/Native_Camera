8c3dc495d58ed60c256edfb802f4a165a8f556ce
rohith <rohith.reddy@codetru.com>
2023-08-01 22:34:55 +0530

    Added the user locations

---
+++ lib/providers/user_places.dart
@@ -0,0 +1,14 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:nativecamera/model/place.dart';
+import 'dart:io';
+class UserPlacesNotifier extends StateNotifier<List<Place>> {
+  UserPlacesNotifier() : super(const []);
+  void addPlace(String title, File image) {
+    final newPlace = Place(title: title, image: image);
+    state = [newPlace, ...state];
+  }
+}
+
+final userPlacesProvider = StateNotifierProvider<UserPlacesNotifier , List<Place>>(
+  (ref) => UserPlacesNotifier(),
+);
---
+++ lib/screens/places.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:nativecamera/providers/user_places.dart';
+import 'package:nativecamera/screens/add_place.dart';
+import 'package:nativecamera/widgets/places_list.dart';
+
+class PlacesScreen extends ConsumerWidget {
+  const PlacesScreen({super.key});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final userPlaces = ref.watch(userPlacesProvider);
+
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Your Places'),
+        actions: [
+          IconButton(
+            icon: const Icon(Icons.add),
+            onPressed: () {
+              Navigator.of(context).push(
+                MaterialPageRoute(
+                  builder: (ctx) => const AddPlaceScreen(),
+                ),
+              );
+            },
+          ),
+        ],
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: PlacesList(
+          places: userPlaces,
+        ),
+      ),
+    );
+  }
+}
