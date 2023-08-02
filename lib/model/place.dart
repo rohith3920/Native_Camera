42a5fadb3e3060a64a2829a5718b51d6d195f3cd
rohith <rohith.reddy@codetru.com>
2023-08-02 22:15:42 +0530

    added the mapping location

--- lib/model/place.dart
+++ lib/model/place.dart
@@ -3,9 +3,21 @@ import 'dart:io';
 
 const uuid = Uuid();
 
+class PlaceLocation {
+  const PlaceLocation({
+    required this.address,
+    required this.latitude,
+    required this.longitude,
+  });
+  final double latitude;
+  final double longitude;
+  final String address;
+}
+
 class Place {
-  Place({required this.title, required this.image}) : id = uuid.v4();
+  Place({required this.title, required this.image, required this.location}) : id = uuid.v4();
   final String id;
   final String title;
   final File image;
+  final PlaceLocation location;
 }
--- lib/widgets/location_input.dart
+++ lib/widgets/location_input.dart
@@ -1,5 +1,9 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import 'package:location/location.dart';
+import 'package:http/http.dart' as http;
+import 'package:nativecamera/model/place.dart';
 
 class LocationInput extends StatefulWidget {
   const LocationInput({super.key});
@@ -9,8 +13,17 @@ class LocationInput extends StatefulWidget {
 }
 
 class _LocationInputState extends State<LocationInput> {
-  Location? _pickedLocation;
+  PlaceLocation? _pickedLocation;
   var _isGettingLocation = false;
+  String get locationImage {
+    if (_pickedLocation == null) {
+      return '';
+    }
+    final lat = _pickedLocation!.latitude;
+    final lng = _pickedLocation!.longitude;
+    return 'https://maps.googleapis.com/maps/api/staticmap?center=$lat,$lng&zoom=16&size=600x300&maptype=roadmap&markers=color:red%7Clabel:A%7C4$lat,$lng&key=YOUR_API_KEY&signature=YOUR_SIGNATURE';
+  }
+
   void _getCurrentLocation() async {
     Location location = Location();
 
@@ -39,12 +52,30 @@ class _LocationInputState extends State<LocationInput> {
     });
 
     locationData = await location.getLocation();
+    final lat = locationData.latitude;
+    final lng = locationData.longitude;
+
+    if (lat == null || lng == null) {
+      return;
+    }
+
+    final url = Uri.parse(
+        'https://maps.googleapis.com/maps/api/geocode/json?latlng=$lat,$lng&key=YOUR_API_KEY');
+
+    final response = await http.get(url);
+
+    final resData = json.decode(response.body);
+
+    final address = resData['results'][0]['formatted_address'];
 
     setState(() {
+      _pickedLocation = PlaceLocation(
+        address: address,
+        latitude: lat,
+        longitude: lng,
+      );
       _isGettingLocation = false;
     });
-    print(locationData.latitude);
-    print(locationData.longitude);
   }
 
   @override
@@ -56,6 +87,15 @@ class _LocationInputState extends State<LocationInput> {
             color: Theme.of(context).colorScheme.onBackground,
           ),
     );
+
+    if (_pickedLocation != null) {
+      previewContent = Image.network(
+        locationImage,
+        fit: BoxFit.cover,
+        width: double.infinity,
+        height: double.infinity,
+      );
+    }
 
     if (_isGettingLocation) {
       previewContent = const CircularProgressIndicator();
